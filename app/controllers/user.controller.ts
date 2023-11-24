10655851c5acfa4d5bde4fd601133664b1fa9943
whoisbel <mjabelcina@gmail.com>
2023-11-24 21:01:25 +0800

    Added login feature

--- app/controllers/user.controller.ts
+++ app/controllers/user.controller.ts
@@ -1,7 +1,8 @@
 import { db } from "../models";
 import { Response, Request } from 'express';
 import bcrypt from 'bcryptjs';
-
+import { JWT_SECRET } from "../configs/jwt.config";
+import jwt from 'jsonwebtoken'
 export async function register(req: Request, res: Response) {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -16,6 +17,20 @@ export async function register(req: Request, res: Response) {
         await user.save();
         res.status(200).json({ message: 'User registered successfully' });
     } catch (error: any) {
+        res.status(403).json({ message: error.message });
+    }
+}
+
+
+export async function login(req: Request, res: Response){
+    try{
+        const User = await db.user.findOne({email: req.body.email})
+        if(!JWT_SECRET){
+            throw new Error('JWT_SECRET environment variable is required');
+        }
+        const token = jwt.sign({ userId: User?.email }, JWT_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ message: 'User login successfully', token});
+    }catch(error:any){
         res.status(403).json({ message: error.message });
     }
 }
--- app/routes/api.endpoint.ts
+++ app/routes/api.endpoint.ts
@@ -3,9 +3,12 @@ import { register } from '../controllers/user.controller';
 import { add_project, view_project } from '../controllers/project.controller';
 import { validateUserInput } from '../middlewares/register.validation';
 import { validateProjectInput } from '../middlewares/project.validation';
+import { login } from '../controllers/user.controller';
+import { validateLogin } from '../middlewares/login.validation';
 const router = express.Router();
 
 router.post('/register', validateUserInput, register);
 router.post('/project', validateProjectInput, add_project);
 router.get('/project', view_project)
+router.post('/login', validateLogin, login)
 export default router;
